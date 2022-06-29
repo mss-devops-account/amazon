ce81ab7d365b62b9e79e28a62f5eaec2c5c39185
nakulyadav123 <nakulmuthyala44@gmail.com>
2022-06-29 19:12:12 +0530

    new and final commit

--- DBUtils.java
+++ DBUtils.java
@@ -1,1 +1,3 @@
 this file contains the DB connection logic
+
+this is also like nakul java file 
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 this file contains the deployment script..
+
+something to say but donno anything 
+and want o write someting but i cant do that 
