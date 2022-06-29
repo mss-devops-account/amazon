c8c7d43e691a9c12a7ab94367e7a7e01fb305ed9
nakulyadav123 <nakulmuthyala44@gmail.com>
2022-06-29 19:05:54 +0530

    updated

--- nakul.java
+++ nakul.java
@@ -1,3 +1,6 @@
 don`worry bro  we will get job soon
 
-i am  married and am getting a baby in 4months 
+i am  married and am getting a baby in 4months
+ asal deni gurinchi alochinchakunda coarse complete cheddam job oste anni avve set avtay ok na ra 
+ kani nta peaceful ga unte antha manchidi ok na 
+
