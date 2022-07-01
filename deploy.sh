0f3168b666341566c740457403deb8996429685e
nakulyadav123 <105660866+nakulyadav123@users.noreply.github.com>
2022-07-01 14:56:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ this file contains the deployment script..
 something to say but donno anything 
 and want to write someting but i cant do that 
 jxjkbasjbxj
+noone will come to save you except urself
+so becareful about ur life
